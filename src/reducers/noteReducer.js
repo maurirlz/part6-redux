404728cdb6147ceaa632d48981125e23c74ce009
maaush <mauribenitez95@gmail.com>
2020-07-05 06:00:00 -0300

    add tests

---
+++ src/reducers/noteReducer.js
@@ -0,0 +1,20 @@
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.data];
+    case 'TOGGLE_IMPORTANCE':
+      const id = action.data.id;
+      const noteToFind = state.find((note) => note.id === id);
+
+      const changedNote = {
+        ...noteToFind,
+        important: !noteToFind.important,
+      };
+
+      return state.map((note) => (note.id !== id ? note : changedNote));
+  }
+
+  return state;
+};
+
+export default noteReducer;
