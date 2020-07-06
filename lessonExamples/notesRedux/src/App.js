414413fe921b79ee1090bd170d05ec8d7df148b5
maaush <mauribenitez95@gmail.com>
2020-07-06 03:08:09 -0300

    finish lesson 6A, starting last exercises from thi slesson

--- lessonExamples/notesRedux/src/App.js
+++ lessonExamples/notesRedux/src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
-import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
-import { useSelector, useDispatch } from "react-redux";
+import Notes from "./Components/Notes";
+import NewNote from "./Components/NewNote";
 
 const App = () => {
-  const dispatch = useDispatch(); // Provides any react component access to the dispatch function of the redux store defined in index.js
-  const notes = useSelector((state) => state); // selects the whole state from the store thanks to Provider
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-
-    dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id));
-  };
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   );
 };
